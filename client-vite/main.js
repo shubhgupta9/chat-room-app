bfce4cdbd5a633fb9b3756e8fef0a48129af4dfa
Shubh Gupta <shubh.g0279@gmail.com>
2022-02-04 23:30:23 +0530

    heroku9

--- client-vite/main.js
+++ client-vite/main.js
@@ -1,5 +1,5 @@
-// import { io } from "socket.io-client";
-const { io } = require("socket.io-client");
+import { io } from "socket.io-client";
+
 const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 const form = document.getElementById("send-container");
